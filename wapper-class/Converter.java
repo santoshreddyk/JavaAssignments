a36fd14aa8f73bd9c386f3241879506296f6e672
santoshreddyk <santoshreddyk@users.noreply.github.com>
2016-07-19 17:25:38 -0500

    Add files via upload

---
+++ wapper-class/Converter.java
@@ -0,0 +1,30 @@
+package com.myc.wrapper;
+
+public class Converter {
+	 Integer convertWrapper(int a)
+	 {
+		 Integer io=new Integer(a);
+		 return a;
+	 }
+	 Float convertWrapper(float a)
+	 {
+		 Float f=new Float(a);
+		 return a;
+	 }
+	  Short convertWrapper(short a)
+	 {
+		 Short s=new Short(a);
+		 return a;
+	 }
+	 Double convertWapper(double a)
+	 {
+		 Double d=new Double(a);
+		 return a;
+	 }
+	 Character convertWapper(char a)
+	 {
+		 Character ch=new Character(a);
+		 return a;
+	 }
+
+}
---
+++ wapper-class/converterEx.java
@@ -0,0 +1,11 @@
+package com.myc.wrapper;
+
+public class converterEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Converter c=new Converter();
+c.convertWrapper(10);
+	}
+
+}
